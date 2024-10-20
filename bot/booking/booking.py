19d1514be45e57f46501ad86bb70d8acff5db5c1
Md. Sifat Kamal <104722062+sifatkamal@users.noreply.github.com>
2024-10-20 17:12:26 +0600

    added manager protocol

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -2,22 +2,28 @@ import os
 import booking.constants as const
 from selenium import webdriver
 
-class Booking(webdriver.Chrome):
+class Booking:
 
-    def __init__(self, driver_path = r"C:/SeleniumDrivers"):
+    def __init__(self, driver_path = r"C:/SeleniumDrivers", teardown = False):
 
         self.driver_path = driver_path
+        self.teardown = teardown
 
         os.environ['PATH'] += self.driver_path
 
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        driver = webdriver.Chrome(options=options)
+        self.driver = webdriver.Chrome(options=options)
 
         super(Booking, self).__init__()
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        
+        if self.teardown == True:
+            self.quit()
+
     def land_first_page(self):
 
-        self.get(const.BASE_URL)
+        self.driver.get(const.BASE_URL)
 
--- bot/run.py
+++ bot/run.py
@@ -1,6 +1,13 @@
 from booking.booking import Booking
 
-inst = Booking()
+# inst = Booking()
 
-inst.land_first_page()
+# inst.land_first_page()
 
+# Eshob kisur bodole bot naam e ekta kisu use kora jay
+
+with Booking() as bot:
+
+    bot.land_first_page()
+
+    print("Exiting")
